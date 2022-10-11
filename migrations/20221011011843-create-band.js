f66be47a01c04ceaf243daab1ad867f5df975b39
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-10-10 22:08:53 -0400

    removed timpestamps

--- migrations/20221011011843-create-band.js
+++ migrations/20221011011843-create-band.js
@@ -1,40 +1,41 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Bands', {
-      id: {
+    await queryInterface.createTable("bands", {
+      band_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      band_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       genre: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       available_start_time: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false,
       },
       end_time: {
-        type: Sequelize.DATE
-      },
-      createdAt: {
+        type: Sequelize.DATE,
         allowNull: false,
-        type: Sequelize.DATE
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+      // removed timestamps
+      //   createdAt: {
+      //     allowNull: false,
+      //     type: Sequelize.DATE,
+      //   },
+      //   updatedAt: {
+      //     allowNull: false,
+      //     type: Sequelize.DATE,
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bands');
-  }
+    await queryInterface.dropTable("bands");
+  },
 };
