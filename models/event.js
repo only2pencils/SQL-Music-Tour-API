7e4879773a547eaef687f2e5a74e058df1816305
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-10-15 14:44:07 -0400

    updated all model files

--- models/event.js
+++ models/event.js
@@ -1,13 +1,30 @@
 "use strict";
-const { Model, DataTypes } = require("sequelize");
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    static associate({ Stage, StageEvent }) {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifestyle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Stage, StageEvent, MeetGreet, SetTime }) {
       // stages
       Event.belongsToMany(Stage, {
         foreignKey: "event_id",
         as: "stages",
         through: StageEvent,
+      });
+
+      // meet and greets
+      Event.hasMany(MeetGreet, {
+        foreignKey: "event_id",
+        as: "meet_greets",
+      });
+
+      // set times
+      Event.hasMany(SetTime, {
+        foreignKey: "event_id",
+        as: "set_times",
       });
     }
   }
--- models/stage.js
+++ models/stage.js
@@ -2,12 +2,22 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Stage extends Model {
-    static associate({ Event, StageEvent }) {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Event, StageEvent, SetTime }) {
       // events
       Stage.belongsToMany(Event, {
         foreignKey: "stage_id",
         as: "events",
         through: StageEvent,
+      });
+      //set times
+      Stage.hasMany(SetTime, {
+        foreignKey: "stage_id",
+        as: "set_times",
       });
     }
   }
